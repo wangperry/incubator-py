74b97d4a68c9cceb894c36ef7399b68872a0234b
Maksym <stepanenkomaksi@gmail.com>
2013-12-03 22:02:06 +0200

    tt

--- algorithms-py/algorithms.py
+++ algorithms-py/algorithms.py
@@ -1,16 +1,34 @@
 
-import win32api, win32con
+var = '''
+Print all valid parentnesis
+'''
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+def print_parenthesis(n):
+    arr = ['' for _ in range(2*n)]
+    gen(n, arr)
+
+
+def gen(n, arr, index=0, left=0, right=0):
+
+    if index == 2*n:
+        print(''.join(arr))
+        return
+
+    if left < n:
+        arr[index] = "("
+        gen(n, arr, index+1, left+1, right)
+
+    if right < left:
+        arr[index] = ")"
+        gen(n, arr, index+1, left, right+1)
+
 
 def main():
-    
 
-    click(10, 10)
-    
+    n = 4
+    print_parenthesis(n)
+
     print("main done")
     
 
