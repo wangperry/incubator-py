f9308de30163b6a914a0546f34735cc744515a9d
mstepan <stepanenkomaksi@gmail.com>
2013-12-19 11:23:08 +0200

    ttt

--- algorithms-py/algorithms.py
+++ algorithms-py/algorithms.py
@@ -8,9 +8,16 @@ def main():
     a = [2, 4, 7, 9, 12, 13, 15, 22]
     b = [4, 5, 6, 7, 9, 12, 17, 18]
 
-    for k in range(1, len(a) + len(b) + 1):
-        order_stat = find_order_stat(a, b, k)
-        print "%s-th element: %s" % (k, order_stat)
+
+    i = 1
+    j = len(a) + len(b) + 1
+
+    for k in range(i, j):
+
+        order_stat1 = find_order_stat_bruteforce(a, b, k)
+        order_stat2 = find_order_stat(a, b, k)
+
+        print "%s-th, bruteforce: %s, optimal: %s" % (k,order_stat1, order_stat2)
 
     print "Main done"
 
--- algorithms-py/interview/task_11_8.py
+++ algorithms-py/interview/task_11_8.py
@@ -3,11 +3,39 @@ Searching in two sorted arrays.
 """
 
 
-def find_order_stat(arr1, arr2, k):
-    """
-    Bruteforce approach
+
+
+def find_order_stat(a, b, k):
     """
 
+    Given sorted arrays, you know that the nth largest will appear somewhere before or at A[n-1] if it is in array A,
+    or B[n-1] if it is in array B
+
+    Consider item at index a in A and item at index b in B.
+
+    Perform binary search as follows
+
+    If a + b > n, then reduce the search set
+        if A[a] > B[b] then b = b / 2, else a = a / 2
+
+    If a + b < n, then increase the search set
+        if A[a] > B[b] then b = 3/2 * b, else a = 3/2 * a (halfway between a and previous a)
+
+    If a + b = n then the nth largest is max(A[a], B[b])
+
+    """
+
+    validate_params(a, b, k)
+
+    if k == 1:
+        return min(a[0], b[0])
+
+    #todo finish this
+
+    return None
+
+
+def validate_params(arr1, arr2, k):
     if k <= 0:
         raise ValueError("Negative or zero order statistic index passed '%s'" % k)
 
@@ -15,6 +43,14 @@ def find_order_stat(arr1, arr2, k):
 
     if k > max_order_stat_index:
         raise ValueError("Too big order statistic index passed, should be less or equals to '%s'" % max_order_stat_index)
+
+
+def find_order_stat_bruteforce(arr1, arr2, k):
+    """
+    Bruteforce approach
+    """
+
+    validate_params(arr1, arr2, k)
 
     i = 0
     j = 0
